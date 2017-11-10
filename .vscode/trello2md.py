39a8ebfcd5dce63c7e2ed641874ab0ad378f566f
zxyxx <379951650@qq.com>
2017-11-10 21:34:31 +0800

    almost done

--- .vscode/trello2md.py
+++ .vscode/trello2md.py
@@ -3,25 +3,65 @@
 import json
 import sys
 
+class Text:
+    def __init__(self, card_name, content):
+        self.card_name = card_name
+        self.content = content
+
+class Card:
+    def __init__(self, card_name, desc, text, list_id):
+        self.card_name = card_name
+        self.desc = desc
+        self.text = text
+        self.list_id = list_id
+
+class Lists:
+    def __init__(self, list_name, card):
+        self.list_name = list_name
+        self.card = card
+
 def get_title(data):
     return data['name']
 
-def get_cards_name(data):
-    output = []
-    cards = data['lists']
-    for each_card in cards:
-        output.append(each_card['name'])
-    return output
+def get_all_lists(data, all_cards):
+    out = []
+    for each_list in data['lists']:
+        list_card = []
+        list_name = each_list['name']
+        list_id = each_list['id']
+        for each_card in all_cards:
+            if each_card.list_id == list_id:
+               list_card.append(each_card) 
+        this_list = Lists(list_name, list_card)
+        out.append(this_list)
+    return out
 
-def get_card_content(data, card_name):
-    output = {}
-    contents = data['actions']
-    for each_content in contents:
-        print each_content['data']['list']['name']
-        # if each_content['data']['list']['name'] == card_name:
-        #     print each_content['data']['card']['name']
-            # output[each_content['data']['card']['name']] = output[each_content['data']['text']]
-    # return output
+def get_all_cards(data, all_texts):
+    out = []
+    for each_card in data['cards']:
+        list_id = each_card['idList']
+        desc = each_card['desc']
+        card_name = each_card['name']
+        card_text = []
+        for each_text in all_texts:
+            if each_text.card_name == card_name:
+                card_text.append(each_text)
+        this_card = Card(card_name, desc, card_text, list_id)
+        out.append(this_card)
+    return out
+
+def get_all_texts(data):
+    out = []
+    for each_text in data['actions']:
+        try:
+            each_text['data']['text']
+        except:
+            pass
+        else:
+            this_text = Text(each_text['data']['card']['name'], each_text['data']['text'])
+            out.append(this_text)
+    return out
+
 
 def trello2md():
     jsonfile = 'exported.json'
@@ -29,11 +69,17 @@ def trello2md():
     with open(jsonfile, 'r') as f:
         data = json.load(f)
         title = get_title(data)
-        all_cards = get_cards_name(data)
-        print all_cards
+        all_text = get_all_texts(data)
+        all_cards = get_all_cards(data, all_text)
+        all_lists = get_all_lists(data, all_cards)
 
-    with open(outfile, 'w') as f:
-        pass
+    for each_list in all_lists:
+        print "## " + each_list.list_name
+        for each_card in each_list.card:
+            print "### " + each_card.card_name
+
+    # with open(outfile, 'w') as f:
+    #     pass
 
 
 
